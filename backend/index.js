56be23f9dfc74436904c73e2194bd25808087369
daveyloder <davidloder92@gmail.com>
2022-02-14 10:59:31 -0500

    backend redo

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,7 @@
-const express = require('express')
+const express = require("express");
 const app = express()
 const PORT = process.env.PORT || 3001
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World! ')
-})
-
-app.get('/api', (req, res) =>{
-    res.json({ message: "Hello from api!"})
-})
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+app.listen(PORT, () =>{
+    console.log(`Server is listening on port ${PORT}`)
 })
