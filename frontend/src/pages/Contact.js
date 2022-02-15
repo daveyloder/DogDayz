6573bf2177b3fe41b57367ff3e50f1e3ceb3b739
daveyloder <davidloder92@gmail.com>
2022-02-15 12:52:49 -0500

    Formatting Contact form

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,38 +1,46 @@
 import React from "react";
-import { Button, Container, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Container, Col, Form, FormGroup, Label, Input } from "reactstrap";
 
 const Contact = () => {
   return (
     <Container className="p-5">
       <Form>
-        <FormGroup>
-          <Label for="contactName">Name</Label>
-          <Input
-            type="text"
-            name="contactName"
-            id="contactName"
-            placeholder="Please enter your name."
-          />
-          <Label for="contactEmail">Email</Label>
-          <Input
-            type="email"
-            name="contactEmail"
-            id="contactEmail"
-            placeholder="Please enter your email."
-            />
-            <Label for="contactPhone">Phone Number</Label>
+        <FormGroup row>
+          <Label sm={2} for="contactName">Name</Label>
+          <Col sm={10}>
             <Input
-              type="phone"
-              name="contactPhone"
-              id="contactPhone"
-              placeholder="Please enter your phone number."
+              type="text"
+              name="contactName"
+              id="contactName"
+              placeholder="Please enter your name."
+            />
+          </Col>
+          <Label sm={2} for="contactEmail">Email</Label>
+          <Col sm={10}>
+            <Input
+              type="email"
+              name="contactEmail"
+              id="contactEmail"
+              placeholder="Please enter your email."
               />
-              <Label for="contactMessage">Message</Label>
+          </Col>
+            <Label sm={2} for="contactPhone">Phone Number</Label>
+            <Col sm={10}>
               <Input
-                type="textarea"
-                name="contactMessage"
-                id="contactMessage"
+                type="phone"
+                name="contactPhone"
+                id="contactPhone"
+                placeholder="Please enter your phone number."
                 />
+            </Col>
+              <Label sm={2} for="contactMessage">Message</Label>
+              <Col sm={10}>
+                <Input
+                  type="textarea"
+                  name="contactMessage"
+                  id="contactMessage"
+                  />
+              </Col>
         </FormGroup>
         <Button>Submit</Button>
       </Form>
